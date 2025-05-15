2ebba4a265b21c0b2be49a6a59f515900089276f
Atul <atulbariyar511@gmail.com>
2025-05-15 19:29:17 +0530

    Initial commit

---
+++ src/main/java/com/shortU/ShortUApplication.java
@@ -0,0 +1,13 @@
+package com.shortU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortUApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shortU/ShortUApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shortU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
